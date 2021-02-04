3b457449c517defa1e9bf4196c1af5774dec140c
zbduan <769419804@qq.com>
2021-02-04 14:46:16 +0800

    标题图片调整

--- src/views/main.vue
+++ src/views/main.vue
@@ -1,115 +1,116 @@
 <template>
-
-    <el-form label-position='left' hide-required-asterisk :model="ruleForm" :rules="rules" ref="ruleForm" label-width="200px" class="demo-ruleForm">
-        <el-divider></el-divider>
-        <div class="block">
-            <el-image :src="require('../assets/title.png')"></el-image>
-        </div>
-        <el-divider></el-divider>
-        <el-form-item label="工号" prop="workCode">
-        </el-form-item>
-        <el-autocomplete
-                v-model="ruleForm.workCode"
-                :fetch-suggestions="querySearchAsync"
-                placeholder="请输入您的工号"
-                @select="handleWorkCode" style="width: 100%"
-        ></el-autocomplete>
-        <el-form-item label="姓名" prop="name">
-        </el-form-item>
-        <el-input v-model="ruleForm.name" placeholder="请输入您的姓名" style="width: 100%" clearable></el-input>
-        <div v-show="false">
-            <el-form-item label="部门" prop="depart">
+    <el-container>
+        <el-form label-position='left' hide-required-asterisk :model="ruleForm" :rules="rules" ref="ruleForm" label-width="200px" class="demo-ruleForm">
+            <div class="block">
+                <el-image :src="require('../assets/title.png')"></el-image>
+            </div>
+            <el-divider></el-divider>
+            <el-form-item label="工号" prop="workCode">
             </el-form-item>
-            <el-input v-model="ruleForm.depart" placeholder="请选择您所在部门" style="width: 100%" clearable readonly></el-input>
-        </div>
-        <el-form-item label="手机" prop="mobile">
-        </el-form-item>
-        <el-input v-model="ruleForm.mobile" placeholder="请输入您的手机号" style="width: 100%" clearable></el-input>
-        <el-form-item label="籍贯" prop="regionChoose">
-        </el-form-item>
-        <el-cascader
-                v-model="ruleForm.regionChoose"
-                :options="regionOptions"
-                @change="regionChange" style="width: 100%"></el-cascader>
-        <el-form-item label="过年地与籍贯是否一致" prop="regionState">
-        </el-form-item>
-        <el-radio-group v-model="ruleForm.regionState">
-            <el-radio label="是"></el-radio>
-            <el-radio label="否"></el-radio>
-        </el-radio-group>
-        <div v-if="ruleForm.regionState == '否'">
-            <el-form-item label="过年所在地" prop="livePlaceChoose" >
+            <el-autocomplete
+                    v-model="ruleForm.workCode"
+                    :fetch-suggestions="querySearchAsync"
+                    placeholder="请输入您的工号"
+                    @select="handleWorkCode" style="width: 100%"
+            ></el-autocomplete>
+            <el-form-item label="姓名" prop="name">
+            </el-form-item>
+            <el-input v-model="ruleForm.name" placeholder="请输入您的姓名" style="width: 100%" clearable></el-input>
+            <div v-show="false">
+                <el-form-item label="部门" prop="depart">
+                </el-form-item>
+                <el-input v-model="ruleForm.depart" placeholder="请选择您所在部门" style="width: 100%" clearable readonly></el-input>
+            </div>
+            <el-form-item label="手机" prop="mobile">
+            </el-form-item>
+            <el-input v-model="ruleForm.mobile" placeholder="请输入您的手机号" style="width: 100%" clearable></el-input>
+            <el-form-item label="籍贯" prop="regionChoose">
             </el-form-item>
             <el-cascader
-                    v-model="ruleForm.livePlaceChoose"
+                    v-model="ruleForm.regionChoose"
                     :options="regionOptions"
-                    @change="livePlaceChange" style="width: 100%"></el-cascader>
-        </div>
-        <el-form-item label="假期行程" prop="travelPlan">
-        </el-form-item>
-        <el-radio-group v-model="ruleForm.travelPlan">
-            <el-radio label="在家"></el-radio>
-            <el-radio label="外地"></el-radio>
-        </el-radio-group>
-        <div v-if="ruleForm.travelPlan == '外地'">
-            <el-form-item label="是否途径中高风险地区" prop="highRiskAreaPass">
+                    @change="regionChange" style="width: 100%"></el-cascader>
+            <el-form-item label="过年地与籍贯是否一致" prop="regionState">
             </el-form-item>
-            <el-radio-group v-model="ruleForm.highRiskAreaPass">
+            <el-radio-group v-model="ruleForm.regionState">
                 <el-radio label="是"></el-radio>
                 <el-radio label="否"></el-radio>
             </el-radio-group>
-        </div>
-        <!--<el-input v-model="ruleForm.travelPlan" style="width: 100%"></el-input>-->
-        <el-form-item label="身体状态" prop="status">
-        </el-form-item>
-        <el-radio-group v-model="ruleForm.status">
-            <el-radio label="正常"></el-radio>
-            <el-radio label="异常"></el-radio>
-        </el-radio-group>
-        <div v-if="ruleForm.status == '异常'">
-            <el-form-item label="身体状态" prop="health">
+            <div v-if="ruleForm.regionState == '否'">
+                <el-form-item label="过年所在地" prop="livePlaceChoose" >
+                </el-form-item>
+                <el-cascader
+                        v-model="ruleForm.livePlaceChoose"
+                        :options="regionOptions"
+                        @change="livePlaceChange" style="width: 100%"></el-cascader>
+            </div>
+            <el-form-item label="假期行程" prop="travelPlan">
             </el-form-item>
-            <el-checkbox-group v-model="ruleForm.healthGroup" @change="handleHealth">
-                <el-checkbox label="咳嗽" name="health"></el-checkbox>
-                <el-checkbox label="感冒" name="health"></el-checkbox>
-                <el-checkbox label="发烧" name="health"></el-checkbox>
-            </el-checkbox-group>
-        </div>
-        <div v-if="ruleForm.status == '异常'">
-            <el-form-item label="是否接触过中高风险地区人员" prop="isHighPass">
-            </el-form-item>
-            <el-radio-group v-model="ruleForm.isHighPass">
-                <el-radio label="是"></el-radio>
-                <el-radio label="否"></el-radio>
+            <el-radio-group v-model="ruleForm.travelPlan">
+                <el-radio label="在家"></el-radio>
+                <el-radio label="外地"></el-radio>
             </el-radio-group>
-        </div>
-        <div v-if="ruleForm.status == '异常'">
-            <el-form-item label="是否居家观察\隔离" prop="isolation">
+            <div v-if="ruleForm.travelPlan == '外地'">
+                <el-form-item label="是否途径中高风险地区" prop="highRiskAreaPass">
+                </el-form-item>
+                <el-radio-group v-model="ruleForm.highRiskAreaPass">
+                    <el-radio label="是"></el-radio>
+                    <el-radio label="否"></el-radio>
+                </el-radio-group>
+            </div>
+            <!--<el-input v-model="ruleForm.travelPlan" style="width: 100%"></el-input>-->
+            <el-form-item label="身体状态" prop="status">
             </el-form-item>
-            <el-radio-group v-model="ruleForm.isolation">
-                <el-radio label="是"></el-radio>
-                <el-radio label="否"></el-radio>
+            <el-radio-group v-model="ruleForm.status">
+                <el-radio label="正常"></el-radio>
+                <el-radio label="异常"></el-radio>
             </el-radio-group>
-        </div>
-        <el-form-item label="返厂日期" prop="returnDate">
-        </el-form-item>
-        <el-date-picker type="date" placeholder="选择返厂日期" v-model="ruleForm.returnDate" value-format="yyyy-MM-dd" style="width: 100%;"></el-date-picker>
-        <el-form-item label="返厂方式" prop="transport">
-        </el-form-item>
-        <el-radio-group v-model="ruleForm.transport">
-            <el-radio label="自驾"></el-radio>
-            <el-radio label="公共交通"></el-radio>
-        </el-radio-group>
-        <el-form-item label="备注" prop="remark">
-        </el-form-item>
-        <el-input type="textarea" v-model="ruleForm.remark"></el-input>
-        <el-form-item label="填写日期" prop="fillDate">
-        </el-form-item>
-        <el-date-picker type="date" placeholder="选择填写日期" v-model="ruleForm.fillDate" value-format="yyyy-MM-dd" style="width: 100%;"></el-date-picker>
-        <el-form-item></el-form-item>
-        <el-button type="primary" @click="submitForm('ruleForm')">提交</el-button>
-        <el-button @click="resetForm('ruleForm')">重置</el-button>
-    </el-form>
+            <div v-if="ruleForm.status == '异常'">
+                <el-form-item label="身体状态" prop="health">
+                </el-form-item>
+                <el-checkbox-group v-model="ruleForm.healthGroup" @change="handleHealth">
+                    <el-checkbox label="咳嗽" name="health"></el-checkbox>
+                    <el-checkbox label="感冒" name="health"></el-checkbox>
+                    <el-checkbox label="发烧" name="health"></el-checkbox>
+                </el-checkbox-group>
+            </div>
+            <div v-if="ruleForm.status == '异常'">
+                <el-form-item label="是否接触过中高风险地区人员" prop="isHighPass">
+                </el-form-item>
+                <el-radio-group v-model="ruleForm.isHighPass">
+                    <el-radio label="是"></el-radio>
+                    <el-radio label="否"></el-radio>
+                </el-radio-group>
+            </div>
+            <div v-if="ruleForm.status == '异常'">
+                <el-form-item label="是否居家观察\隔离" prop="isolation">
+                </el-form-item>
+                <el-radio-group v-model="ruleForm.isolation">
+                    <el-radio label="是"></el-radio>
+                    <el-radio label="否"></el-radio>
+                </el-radio-group>
+            </div>
+            <el-form-item label="返厂日期" prop="returnDate">
+            </el-form-item>
+            <el-date-picker type="date" placeholder="选择返厂日期" v-model="ruleForm.returnDate" value-format="yyyy-MM-dd" style="width: 100%;"></el-date-picker>
+            <el-form-item label="返厂方式" prop="transport">
+            </el-form-item>
+            <el-radio-group v-model="ruleForm.transport">
+                <el-radio label="自驾"></el-radio>
+                <el-radio label="公共交通"></el-radio>
+            </el-radio-group>
+            <el-form-item label="备注" prop="remark">
+            </el-form-item>
+            <el-input type="textarea" v-model="ruleForm.remark"></el-input>
+            <el-form-item label="填写日期" prop="fillDate">
+            </el-form-item>
+            <el-date-picker type="date" placeholder="选择填写日期" v-model="ruleForm.fillDate" value-format="yyyy-MM-dd" style="width: 100%;"></el-date-picker>
+            <el-form-item></el-form-item>
+            <el-button type="primary" @click="submitForm('ruleForm')">提交</el-button>
+            <el-button @click="resetForm('ruleForm')">重置</el-button>
+        </el-form>
+    </el-container>
+
 </template>
 
 <script>
